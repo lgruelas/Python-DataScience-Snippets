c00ac047f719e53b3ec18c3c4743ccfa6f113f96
Germán Ruelas <lgruelas@hotmail.com>
2020-05-20 19:16:54 -0500

    Add stack

---
+++ DS/node.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+#DATA STRUCTURES FOR PYTHON3
+
+class Node:
+    def __init__(self, value, next_node=None):
+        self._value = value
+        self._next_node = next_node
+    
+    def get_value(self):
+        return self._value
+    
+    def get_next_node(self):
+        return self._next_node
+
+    def set_next_node(self, new_node):
+        self._next_node = new_node
---
+++ DS/stack.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3
+
+#DATA STRUCTURES FOR PYTHON3
+
+from node import Node
+
+class FlowException(Exception):
+    pass
+
+class Stack:
+    def __init__(self, limit=100):
+        self._top_item = None
+        self._size = 0
+        self._limit = limit
+
+    def push(self, value):
+        if not self.__has_space():
+            raise FlowException("OverFlow error, the stack is full")
+        new_node = Node(value, self._top_item)
+        self._size += 1
+        self._top_item = new_node
+
+    def pop(self):
+        if self.__is_empty():
+            raise FlowException("UnderFlow error, the stack is empty")
+        to_return = self._top_item.get_value()
+        self._top_item = self._top_item.get_next_node()
+        self._size -= 1
+        return to_return
+    
+    def peek(self):
+        if self.__is_empty():
+            raise FlowException("UnderFlow error, the stack is empty")
+        return self._top_item.get_value()
+
+    def __has_space(self):
+        return self._limit > self._size
+
+    def __is_empty(self):
+        return self._size == 0
